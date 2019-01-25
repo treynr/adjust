c169b27a908a3b646a9637bfc718c50e771791c8
TR <tim@fin0c.net>
2019-01-25 06:40:25 -0600

    Remove unneeded code

--- src/Info.hs
+++ src/Info.hs
@@ -9,7 +9,6 @@
 module Info (
 
    _DESC
- , _DTAG
  , _EXEC
  , _HASH
  , _INFO
@@ -17,10 +16,7 @@ module Info (
 
 ) where
 
-import Data.List          (intercalate)
-import Data.Time          (getCurrentTime, toGregorian, utctDay)
 import Development.GitRev (gitHash)
-import System.Environment (getArgs)
 
 _DESC :: String
 _DESC = "Correct for multiple testing by controlling the family-wise error " ++
@@ -37,29 +33,4 @@ _INFO = _EXEC ++ " v. " ++ _VERS ++ _HASH
 
 _VERS :: String
 _VERS = "1.0.0"
-
--- | Data export tag
--- | Attaches program version info and command line arguments for
--- | reproducibility, as well as the output file creation date.
---
-_DTAG :: String -> IO String
-_DTAG cols = do
-    sargs <- theArgs
-    syear <- theYear
-    smonth <- theMonth
-    sday <- theDay
-
-    return $ intercalate "\n"
-        [ "## " ++ _INFO
-        , "## last updated " ++ syear ++ ('.' : smonth) ++ ('.' : sday)
-        , "## " ++ _EXEC ++ (' ' : sargs)
-        , "#"
-        , cols
-        ]
-    where
-        theDate' = toGregorian . utctDay <$> getCurrentTime
-        theYear = (\(y, _, _) -> show y) <$> theDate'
-        theMonth = (\(_, m, _) -> show m) <$> theDate'
-        theDay = (\(_, _, d) -> show d) <$> theDate'
-        theArgs = foldr (\acc xs -> acc ++ " " ++ xs) "" <$> getArgs
 
